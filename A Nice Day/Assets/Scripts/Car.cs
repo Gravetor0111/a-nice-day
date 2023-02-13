342af97fc9a12bb71f19451bde4bc2714322cd7a
Mihir Desai <19mca153@nirmauni.ac.in>
2023-02-14 00:11:09 +0530

    14-2 (1)

--- A Nice Day/Assets/Scripts/Car.cs
+++ A Nice Day/Assets/Scripts/Car.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Car : MonoBehaviour
 {
 
     public WheelCollider wheelColliderFrontLeft;
