743b96ddf2332d8b4780ac6d4931d18359454fc1
Mihir Desai <19mca153@nirmauni.ac.in>
2023-02-15 00:21:59 +0530

    14-2 (4)

--- A Nice Day/Assets/Scripts/Car.cs
+++ A Nice Day/Assets/Scripts/Car.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+    public Transform centerOfMass;
 
     public WheelCollider wheelColliderFrontLeft;
     public WheelCollider wheelColliderFrontRight;
@@ -17,17 +18,21 @@ public class Car : MonoBehaviour
 
     public float motorTorque = 100f;
     public float maxSteer = 20f;
+    private Rigidbody _rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.centerOfMass = centerOfMass.localPosition;
     }
 
     void FixedUpdate()
     {
-        // wheelColliderBackLeft.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-        // wheelColliderBackRight.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+        wheelColliderFrontLeft.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
+        wheelColliderFrontRight.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
+        wheelColliderBackLeft.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+        wheelColliderBackRight.motorTorque = Input.GetAxis("Vertical") * motorTorque;
     }
 
     // Update is called once per frame
@@ -36,20 +41,20 @@ public class Car : MonoBehaviour
         var pos = Vector3.zero;
         var ros = Quaternion.identity;
 
-        // wheelColliderBackLeft.GetWorldPose(out pos, out ros);
-        // wheelBackLeft.position = pos;
-        // wheelBackLeft.rotation = ros;
+        wheelColliderBackLeft.GetWorldPose(out pos, out ros);
+        wheelBackLeft.position = pos;
+        wheelBackLeft.rotation = ros;
 
-        // wheelColliderBackRight.GetWorldPose(out pos, out ros);
-        // wheelBackRight.position = pos;
-        // wheelBackRight.rotation = ros * Quaternion.Euler(0,180,0);
+        wheelColliderBackRight.GetWorldPose(out pos, out ros);
+        wheelBackRight.position = pos;
+        wheelBackRight.rotation = ros;
         
-        // wheelColliderFrontLeft.GetWorldPose(out pos, out ros);
-        // wheelFrontLeft.position = pos;
-        // wheelFrontLeft.rotation = ros;
+        wheelColliderFrontLeft.GetWorldPose(out pos, out ros);
+        wheelFrontLeft.position = pos;
+        wheelFrontLeft.rotation = ros;
         
-        // wheelColliderFrontRight.GetWorldPose(out pos, out ros);
-        // wheelFrontRight.position = pos;
-        // wheelFrontRight.rotation = ros * Quaternion.Euler(0,180,0);
+        wheelColliderFrontRight.GetWorldPose(out pos, out ros);
+        wheelFrontRight.position = pos;
+        wheelFrontRight.rotation = ros;
     }
 }
