f0fb833766f2eda577ad7656a08831015d7fa0d7
Mihir Desai <19mca153@nirmauni.ac.in>
2023-02-16 00:35:01 +0530

    15-2 (1)

--- A Nice Day/Assets/Scripts/Car.cs
+++ A Nice Day/Assets/Scripts/Car.cs
@@ -5,19 +5,10 @@ using UnityEngine;
 public class Car : MonoBehaviour
 {
     public Transform centerOfMass;
-
-    public WheelCollider wheelColliderFrontLeft;
-    public WheelCollider wheelColliderFrontRight;
-    public WheelCollider wheelColliderBackLeft;
-    public WheelCollider wheelColliderBackRight;
-
-    public Transform wheelFrontLeft;
-    public Transform wheelFrontRight;
-    public Transform wheelBackLeft;
-    public Transform wheelBackRight;
-
-    public float motorTorque = 100f;
+    public float motorTorque = 1500f;
     public float maxSteer = 20f;
+    public float Steer { get; set; }
+    public float Throttle { get; set; }
     private Rigidbody _rigidbody;
 
     // Start is called before the first frame update
@@ -27,34 +18,9 @@ public class Car : MonoBehaviour
         _rigidbody.centerOfMass = centerOfMass.localPosition;
     }
 
-    void FixedUpdate()
-    {
-        wheelColliderFrontLeft.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
-        wheelColliderFrontRight.steerAngle = Input.GetAxis("Horizontal") * maxSteer;
-        wheelColliderBackLeft.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-        wheelColliderBackRight.motorTorque = Input.GetAxis("Vertical") * motorTorque;
-    }
-
     // Update is called once per frame
     void Update()
     {
-        var pos = Vector3.zero;
-        var ros = Quaternion.identity;
-
-        wheelColliderBackLeft.GetWorldPose(out pos, out ros);
-        wheelBackLeft.position = pos;
-        wheelBackLeft.rotation = ros;
-
-        wheelColliderBackRight.GetWorldPose(out pos, out ros);
-        wheelBackRight.position = pos;
-        wheelBackRight.rotation = ros;
         
-        wheelColliderFrontLeft.GetWorldPose(out pos, out ros);
-        wheelFrontLeft.position = pos;
-        wheelFrontLeft.rotation = ros;
-        
-        wheelColliderFrontRight.GetWorldPose(out pos, out ros);
-        wheelFrontRight.position = pos;
-        wheelFrontRight.rotation = ros;
     }
 }
