121d3ba883b91b6ccb0b7b0e975ba00fd8b1c418
Mihir Desai <19mca153@nirmauni.ac.in>
2023-02-16 16:16:31 +0530

    16-2 (1)

--- A Nice Day/Assets/Scripts/GameManager.cs
+++ A Nice Day/Assets/Scripts/GameManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+    public InputController inputController { get; private set; }
+
+    void Awake()
+    {
+        Instance = this;
+        inputController = GetComponentInChildren<InputController>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
