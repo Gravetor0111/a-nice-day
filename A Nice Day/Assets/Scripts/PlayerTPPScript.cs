68c59cbab9d2d907841b4097ab6bad1a32de2b68
Mihir Desai <19mca153@nirmauni.ac.in>
2023-03-08 22:39:21 +0530

    8-3 (1)

--- A Nice Day/Assets/Scripts/PlayerTPPScript.cs
+++ A Nice Day/Assets/Scripts/PlayerTPPScript.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class PlayerTPPScript : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform cam;
+
     public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    public float turnSmoothVelocity;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,7 +27,18 @@ public class PlayerTPPScript : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            controller.Move(direction * speed * Time.deltaTime);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            anim.SetBool("IsMoving", true);
+        }
+
+        else
+        {
+            anim.SetBool("IsMoving", false);
         }
 
     }
